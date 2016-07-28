e6a6127feacf9a21c6d3af2bf6e7e267f8ff65ac
imoutits <imoutits@gmail.com>
2016-07-27 23:59:15 -0700

    pulling in pgoapi

---
+++ pgoapi/utilities.py
@@ -0,0 +1,82 @@
+"""
+pgoapi - Pokemon Go API
+Copyright (c) 2016 tjado <https://github.com/tejado>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author: tjado <https://github.com/tejado>
+"""
+
+import re
+import struct
+import logging
+
+from json import JSONEncoder
+
+# other stuff
+from google.protobuf.internal import encoder
+from geopy.geocoders import GoogleV3
+from s2sphere import Cell, CellId, LatLng
+
+log = logging.getLogger(__name__)
+
+def f2i(float):
+  return struct.unpack('<Q', struct.pack('<d', float))[0]
+
+def f2h(float):
+  return hex(struct.unpack('<Q', struct.pack('<d', float))[0])
+
+def h2f(hex):
+  return struct.unpack('<d', struct.pack('<Q', int(hex,16)))[0]
+
+def to_camel_case(value):
+  return ''.join(word.capitalize() if word else '_' for word in value.split('_'))
+
+# JSON Encoder to handle bytes
+class JSONByteEncoder(JSONEncoder):
+    def default(self, o):
+        return o.decode('utf-8')
+
+def get_pos_by_name(location_name):
+    geolocator = GoogleV3()
+    loc = geolocator.geocode(location_name, timeout=10)
+    if not loc:
+        return None
+
+    log.info("Location for '%s' found: %s", location_name, loc.address)
+    log.info('Coordinates (lat/long/alt) for location: %s %s %s', loc.latitude, loc.longitude, loc.altitude)
+
+    return (loc.latitude, loc.longitude, loc.altitude)
+
+def get_cell_ids(lat, long, radius = 10):
+    origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
+    walk = [origin.id()]
+    right = origin.next()
+    left = origin.prev()
+
+    # Search around provided radius
+    for i in range(radius):
+        walk.append(right.id())
+        walk.append(left.id())
+        right = right.next()
+        left = left.prev()
+
+    # Return everything
+    return sorted(walk)
+
